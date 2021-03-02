423d846a2d7a29cf14c29d7851812eb686285cb2
Jack <muniaxe@gmail.com>
2021-03-02 13:59:41 +0100

    First Commit

---
+++ src/main/java/nextlevel/jpa/demo/entities/Tester.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nextlevel.jpa.demo.entities;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author Jack
+ */
+public class Tester {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
+        EntityManager em = emf.createEntityManager();
+        
+        Person p1 = new Person("Jønke", 1963);
+        Person p2 = new Person("Blodie", 1959);
+        
+        em.getTransaction().begin();
+        em.persist(p1);
+        em.persist(p2);
+        em.getTransaction().commit();
+        
+        System.out.println("p1: " + p1.getP_id() + ", " + p1.getName());
+        System.out.println("p2: " + p2.getP_id() + ", " + p2.getName());
+    }
+    
+    
+}
